b43767adc47a1ebe292ecd700fa6e765ecdeb73a
FernandaSzAdr <fsandrade25@gmail.com>
2019-10-19 23:34:33 -0300

    Adiciona docstrings nos metodos da memoria

--- Arquivos/helpers/memoria.py
+++ Arquivos/helpers/memoria.py
@@ -8,6 +8,11 @@ class Memoria:
 
         self.data[0] = I_node(None, 'r', 1, node_type='dir')
 
+    """
+        Verificará primeiramente se a memória (mesmo com fragmentação)
+        conseguirá armazenar o `file`. Caso tenha espaço disponível, será
+        realizado o `alocate`.
+    """
     def add_file(self, index, file):
         if self.check_storage(file.size):
             self.allocate(file)
@@ -19,6 +24,19 @@ class Memoria:
             return True
         return False
 
+    """
+        Recebendo um `file` que pode ser um diretório ou arquivo, será
+        realizado a alocação desse `file` na memória.
+
+        Percorre-se todas as posições de memória em busca da primeira posição
+        livre. Quando acha-se ela, é inserido uma parte do `file` nesa posição.
+
+        Continuará percorrendo a memória, ate que todo o conteúdo do `file`
+        tenha sido alocado na memória.
+
+        As posições da memória que estão armazenando o `file`, serão salvas no
+        vetor `file.indexes`.
+    """
     def allocate(self, file):
         for i, value in enumerate(self.data):
             if not value:
